b408a8b9f31390384dfc763d4040f5101e55cffe
Felix Benetzeder <felix.benetzeder@blackrock.com>
2019-04-16 18:59:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(sentence)
   if sentence == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif sentence == sentence.upcase
-    return "NO, NOT SINCE 1938!."
+    return "NO, NOT SINCE 1938!"
   else sentence != sentence.upcase
     return "HUH?! SPEAK UP, SONNY!"
   end
