baec87d58b1fa51db9eaae05f2c2d5121795b384
Felix Benetzeder <felix.benetzeder@blackrock.com>
2019-04-16 18:55:58 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,15 @@
+def speak_to_grandma(sentence)
+  if sentence == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif sentence == sentence.upper
+    return "NO, NOT SINCE 1938!."
+  elsif sentence != sentence.upcase
+    return "HUH?! SPEAK UP, SONNY!"
+  else
+    
+end
+
+
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
